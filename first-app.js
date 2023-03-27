da11ed56db68be8cc88fcbd68db6fd0ed7239288
jitujiten <sahuohjitu@gmail.com>
2023-03-27 11:03:33 +0530

    Added second commit of node.js

--- first-app.js
+++ first-app.js
@@ -1,1 +1,40 @@
-console.log("hello world")
+
+
+// var product=(a,b)=>{
+//     console.log(a*b);
+// }
+
+// product(5,7);
+
+
+
+
+// const person={
+//     name:"jitendra sahu",
+//     age:23,
+//     print:()=>{
+//         console.log("hii i am "+this.name)
+//     }
+// }
+// person.print();
+
+// one way:-
+// const person={
+//     name:"jitendra sahu",
+//     age:23,
+//     print:function(){
+//         console.log("hii i am "+this.name)
+//     }
+// }
+// person.print();
+
+
+// 2nd way:-
+const person={
+    name:"jitendra sahu",
+    age:23,
+    print(){
+        console.log("hii i am "+this.name)
+    }
+}
+person.print();
